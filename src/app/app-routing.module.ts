8302cdc802302f93abfec29956e838f5ba7c835e
tr1ble <mrtr1ble@mail.ru>
2020-09-10 11:53:09 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { UserListComponent } from './list/user-list/user-list.component';
+import { UserFormComponent } from './form/user-form/user-form.component';
+ 
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'adduser', component: UserFormComponent }
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
