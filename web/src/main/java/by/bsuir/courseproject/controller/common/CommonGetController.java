b87d8a502d2856a3947d2f616b6e3f940a84fc6d
Tr1ble <mrtr1ble@mail.ru>
2020-09-15 13:27:38 +0300

    Little fix

--- web/src/main/java/by/bsuir/courseproject/controller/common/CommonGetController.java
+++ web/src/main/java/by/bsuir/courseproject/controller/common/CommonGetController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,12 +37,12 @@ public class CommonGetController {
         this.trainerService = trainerService;
     }
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/")
     public  String training() {
         return "Online training application";
     }
 
-    @RequestMapping(value = "/courses")
+    @RequestMapping(value = "/courses", produces = {"application/json"} )
     public  ResponseEntity<List<Course>> getCourses() {
         return ResponseEntity.ok(courseService.getAll());
     }
